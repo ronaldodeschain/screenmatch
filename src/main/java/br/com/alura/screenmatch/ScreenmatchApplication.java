fcb70c4780f813bc9b5fc905305e35ff8f3a043c
ronaldodeschain <lotushall@gmail.com>
2025-07-23 16:50:32 -0300

    [Update] - Criando conversor de dados da Api, colocando jackson nas dependencias

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,30 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumiApi = new ConsumoApi();
+		var json = consumiApi.obterDados("https://www.omdbapi.com/?t=firefly&apikey=534784da");
+//		System.out.println(json);
+//		json = consumiApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
+		System.out.println(dados);
+	}
+
 
 }
